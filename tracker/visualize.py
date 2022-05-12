b1e0625d34a6ee21b57bcd3df955b91cf00ecd4b
rems64 <rems64des@gmail.com>
2022-05-12 02:06:51 +0200

    Fixed vicious bug causing wrong track association Minor tweaks

--- tracker/visualize.py
+++ tracker/visualize.py
@@ -1,28 +1,36 @@
+from inspect import currentframe
 import cv2
 
 import tracker.common as cmn
 from . import utils
 
-def visualize(cap, data: cmn.TrackedData):
+def visualize(source: cmn.TrackingSource, data: cmn.TrackedData):
     frame = 0
     deltaDelayMs = int(1000/float(60))
     colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255), (255, 0, 255)]
     paused = False
     tracks = data.tracks
 
-    dx = cap.get(cv2.CAP_PROP_FRAME_WIDTH)/(2*data.input_width)
-    dy = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)/(2*data.input_height)
+    dx = source.resolution[0]/(2*data.input_width)
+    dy = source.resolution[1]/(2*data.input_height)
     if len(tracks)==0:
         utils.log("No track found")
         return
-    while cap.isOpened():
-        ret, pic = cap.read()
+    shouldExit = False
+    shouldPause = True
+    source.setFrame(source.begin_frame)
+    while not shouldExit:
+        ret, pic = source.readFrame()
+        if not ret or frame > source.frame_count:
+            shouldExit = True
+            break
         if ret:
             sized = cv2.resize(pic, (0, 0), fx=0.5, fy=0.5)
             withTrack = sized.copy()
-            j=0
-            for track in tracks:
-                frames = track.frames
+            displayedTracksIndex=0
+            displayedTracksCount=0
+            for track in range(len(tracks)):
+                frames = tracks[track].frames
                 idx = frame+1
                 while idx>=0:
                     if  idx>=len(frames) or not frames[idx]:
@@ -30,22 +38,23 @@ def visualize(cap, data: cmn.TrackedData):
                         continue
                     if frames[idx].frame_number == frame and not frames[idx].empty:
                         loc = frames[idx].points[0].location
-                        # print(frames[idx].points)
-                        cv2.circle(withTrack, (int(dx*loc[0]), int(dy*loc[1])), 5, colors[j], -1)
-                        cv2.putText(withTrack, str(j), (int(dx*loc[0])+5, int(dy*loc[1])+5), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 1)
-                        j+=1
+                        cv2.circle(withTrack, (int(dx*loc[0]), int(dy*loc[1])), 5, colors[displayedTracksIndex], -1)
+                        cv2.putText(withTrack, str(displayedTracksIndex), (int(dx*loc[0])+5, int(dy*loc[1])+5), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 1)
+                        displayedTracksCount+=1
                         break
                     idx-=1
+                displayedTracksIndex+=1
 
             cv2.putText(withTrack, "Frame " + str(frame), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 1)
-            cv2.putText(withTrack, "Tracks : " + str(j), (sized.shape[1]-200, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 1)
+            cv2.putText(withTrack, "Tracks : " + str(displayedTracksCount), (sized.shape[1]-200, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 1)
             cv2.putText(sized, "Frame " + str(frame), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 1)
             cv2.imshow("Original", sized)
             cv2.imshow("Tracked", withTrack)
             key = cv2.waitKey(deltaDelayMs)
             if key == ord('q'):
                 break
-            elif key == ord('p'):
+            elif key == ord('p') or shouldPause:
+                shouldPause = False
                 paused = True
             if paused:
                 key = cv2.waitKey()
@@ -53,11 +62,11 @@ def visualize(cap, data: cmn.TrackedData):
                     paused = False
                 elif key == ord('l'):
                     frame += 1
-                    cap.set(1, frame)
+                    source.setFrame(frame)
                     continue
                 elif key == ord('j'):
                     frame -= 1
-                    cap.set(1, frame)
+                    source.setFrame(frame)
                     continue
                 elif key == ord('q'):
                     break
